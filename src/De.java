d9eba1ef2701f15a38ab620709928a13fff8ae72
roger <roger.tuyishime@gmail.com>
2022-07-11 13:42:24 +0200

    add Gobelet class

--- src/De.java
+++ src/De.java
@@ -3,32 +3,33 @@
  * @valeur valeur donnée par un dé
  */
 public class De {
-    private int valeur;
+    private int valeurDuDe;
 
     /**
      * le constructeur initialisera la valeur du dé à 0
      */
     public De() {
-        this.valeur = 0;
+        this.valeurDuDe = 0;
     }
 
-    public int getValeur() {
-        return valeur;
+    public int getValeurDuDe() {
+        return valeurDuDe;
     }
 
-    public void setValeur(int valeur) {
-        this.valeur = valeur;
+    public void setValeur(int valeurDuDe) {
+        this.valeurDuDe = valeurDuDe;
     }
 
     /**
      * Methode permettant de simuler le lancé du dé,
      * génère une valeur comprise ente 1 et 6
      */
-    public void lancer() {
+    public int lancerDe() {
         int valeur;
         int minValue = 1;
         int maxValue = 6;
         valeur = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
         setValeur(valeur);
+        return valeur;
     }
 }
